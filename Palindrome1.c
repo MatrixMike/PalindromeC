1396b9eee64c33b773820b51b884ccb0407d81af
matrixmike <matroxmike@icloud.com>
2025-07-13 12:40:46 +1000

    slight cosmetic debug mods

--- Palindrome1.c
+++ Palindrome1.c
@@ -20,6 +20,8 @@
  * 
  * 
  */
+ 
+ // plenty of bugs in this
 #include <stdio.h>
 #include <string.h>
 /*
@@ -30,13 +32,13 @@
  * 
  */
 
-
+  bool s = false;
 int
 main (int argc, char **argv)
 {
 //char vars1[20];
-  char vars2[] = "mimississipxyxpipipzzzz Revitive";
-  char vars3[7] = { 0 };
+  char vars2[] = "Re vitivemimississipxyxpipipzzAz RevitiveXYZ abcdefgfedcba";
+  char vars3[9] = { 0 };
 // char *wikipedia = vars[4];
 
 /*
@@ -50,7 +52,9 @@ main (int argc, char **argv)
   int len = strlen (vars2);
 
   int i;
-  printf ("found1 =, %s\n", vars2);
+
+  printf ("found1 =, %s\n", vars2);   // add field width
+  printf ("          %s\n", "012345678901234567890");
 //    printf ("fo =, %s\n", vars2[1]);
 
 /*
@@ -64,17 +68,31 @@ main (int argc, char **argv)
 
   for (i = 0; i < len; i++)
     {
-      if ((vars2[i] == vars2[i + 2]) && (vars2[i] != vars2[i + 1]))
+      if ((vars2[i] == vars2[i + 2]) && (vars2[i] != vars2[i + 1]))  // three pattern detected
 	{
-	  printf ("found2 =, %d\n", i);
+	  printf ("found3 =, %d\n", i);
+	  		strncpy(vars3,"DDDDD", 6);  // for debugging : 
 	  strncpy (vars3, vars2 + (i), 3);
 //    strncpy(dest, src+(i*4), 4);
+
 	  printf ("loop %d : %s\n", i, vars3);
-	  if ((vars2[i-1] == vars2[i+3]))
+	  if ((vars2[i-1] == vars2[i+3]))  // five pattern detected
 	  {
-		printf ("   five chars =, %d\n", i); 
+		  s = true;  // a five pattern detected
+		printf ("   five chars starting at , %d\n", i-1); 
+		strncpy(vars3,"DDDDD", 5);  // for debugging 
 		strncpy (vars3, &vars2[i]-1, 5);  // vars3 destination ; vars2 source :  vars2[i-1]
 		printf ("loop5 %d : %s\n", i, vars3);
+			  if ((vars2[i-2] == vars2[i+4]))  // five pattern detected
+			  {
+						printf ("   seven chars starting at , %d\n", i-2);  
+				  		strncpy (vars3,"DDDDDEE", 7);  // for debugging 
+						strncpy (vars3, &vars2[i]-2, 7);  // vars3 destination ; vars2 source :  vars2[i-1]
+						printf ("loop7 %d : %s\n", i, vars3);
+			  }
+			else {
+				printf("   seven not found\n");
+			}
 	  }
 	}
     }
